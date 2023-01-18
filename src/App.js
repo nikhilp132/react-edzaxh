79a0b277dcd5b837dd834ebcb6835fecb36a8616
nikhilp132 <85994357+nikhilp132@users.noreply.github.com>
2023-01-18 09:14:06 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -15,10 +15,6 @@ const App = () => {
 
   return (
     <>
-      <div>
-        <h1>Hello StackBlitz!</h1>
-        <p>Start editing to see some magic happen :)</p>
-      </div>
       <h1 className="timer">{currTime}</h1>
     </>
   );
